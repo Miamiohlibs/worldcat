d33199b0af3ad68adbb802220e025112c77a86b2
craigboman <craig.boman@gmail.com>
2019-03-06 16:35:00 -0500

    regex for number var, extracting only int rather than str/alpha/num chars

--- metadata/loopHoldingStatus.py
+++ metadata/loopHoldingStatus.py
@@ -1,7 +1,7 @@
 #purpose of looping through csv list of records
 
 def loopHolding():
-    import csv, holdingStatus
+    import csv, holdingStatus, re
     from holdingStatus import status
 
     with open('../data/sandboxRecords.csv') as file:
@@ -9,19 +9,18 @@ def loopHolding():
 
     for i in data:
         for s in range(len(data)):
-        #check to make sure oclcnumb is correct length
-            number = data[s][0]
-            holding = status(number)
-            print(s,number,holding)
+            number = re.sub("[^0-9]","",data[s][0]) #takes out any characters
 
+                #check to make sure oclcnumb is correct length
+                #if len(number) !=
+            holding = status(number)
+            #if holding = False:
+                #updateHolding(number)
+            #elif holding = True:
+                #Something
+            #else:
+                #nothing
+            print(s,number,holding) #dev testing variables
         if s == len(data)-1:
             break
     print("stop")
-
-
-    #if holding = False:
-        #updateHolding(number)
-    #elif holding = True:
-        #Something
-    #else:
-        #nothing
