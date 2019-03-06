5c9e9df6997fb7852371b0eda5483e5b5285c88a
craigboman <craig.boman@gmail.com>
2019-03-06 14:46:20 -0500

    copied holdingstatus.py; starting to loop and map out set status functions

--- metadata/loopHoldingStatus.py
+++ metadata/loopHoldingStatus.py
@@ -4,13 +4,17 @@ from holdingStatus import status
 with open('../data/sandboxRecords.csv') as file:
     data = list(csv.reader(file))
 
+counter = 0
 for i in data:
     #check to make sure oclcnumb is correct length
+    number = data[counter][0]
+    holding = status(number)
+    print(number,holding)
+    counter += 1 #probably a better way to do counter
 
-    counter = 0
-    status(data[counter][0])
-    counter += 1
-
-    #SUCCESS
-
-    #include if statement that if status != TRUE, send holding update PUT request
+    #if holding = False:
+        #updateHolding(number)
+    #elif holding = True:
+        #Something
+    #Else:
+        #nothing
