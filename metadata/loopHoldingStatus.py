b9e8fe11d3bd3cbad127083daa0f5d927040eb75
craigboman <craig.boman@gmail.com>
2019-03-11 11:31:14 -0400

    re.sub fails; does not like str action on numpy bytes object

--- metadata/loopHoldingStatus.py
+++ metadata/loopHoldingStatus.py
@@ -21,23 +21,23 @@
 #purpose: loop through csv list of records
 
 def loopHolding():
-    import csv, holdingStatus, re
+    import csv, holdingStatus, re, numpy
     from unsetHolding import unset
     from setHolding import set
     from holdingStatus import status
 
-    with open('../data/sandboxRecords.csv') as file:
-        data = list(csv.reader(file))
+    data = numpy.loadtxt(open('../data/sandboxRecords.csv'), delimiter='/n',dtype='int')
 
     for i in data:
         for s in range(len(data)):
             #check to make sure oclcnumber is correct length
-            number = re.sub("[^0-9]","",data[s][0]) #takes out any characters
+            #number errors out; does not like str action on numpy bytes object
+            number = re.sub("[^0-9]","",data[s]) #takes out any characters
             #if len(number) !=
             holding = status(number)
             if holding == False:
                 print(print(s,number,holding,set(number)) #dev testing vars
-            elif holding == True:
+            else holding == True:
                 print(s,number,holding,unset(number)) #dev testing vars
             #else:
                 #nothing
