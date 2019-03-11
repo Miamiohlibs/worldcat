599ecae35830d51e9f29e52448463777b69bcd03
craigboman <craig.boman@gmail.com>
2019-03-11 10:41:09 -0400

    mapping out batch status checks

---
+++ metadata/batchLoopStatus.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ###############################################################################
+# Copyright Miami University Libraries - Craig Boman
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+
+#purpose: loop through csv list of records
+
+def loopHolding():
+    import csv, holdingStatus, re, batchStatus
+    from unsetHolding import unset
+    from setHolding import set
+    from holdingStatus import status
+
+    with open('../data/sandboxRecords.csv') as file:
+        data = list(csv.reader(file))
+
+    for i in data:
+        for s in range(len(data)):
+            #check to make sure oclcnumber is correct length
+            number = re.sub("[^0-9]","",data[s][0]) #takes out any characters
+            #if len(number) !=
+            holding = status(number)
+            if holding == False:
+                print(print(s,number,holding,set(number)) #dev testing vars
+            elif holding == True:
+                print(s,number,holding,unset(number)) #dev testing vars
+            #else:
+                #nothing
+        if s == len(data)-1:
+            break
+    print("stop")
---
+++ metadata/batchStatus.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ###############################################################################
+# Copyright Miami University Libraries - Craig Boman
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License. You may obtain a copy of
+# the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+###############################################################################
+
+# Retrieves status of individual OCLC number from your local/test collection
+# Currently MIA is hardcoded; could replace with arg/kwarg variable if needed
+
+def status(fifty): #list of 50 OCLC numbers
+    import requests, json
+    import get_token
+    from get_token import my_wskey, my_user
+
+    request_url = 'https://worldcat.org/ih/checkholdings?holdingLibraryCode=MIA&oclcNumber='+fifty
+
+    authorization_header = my_wskey.get_hmac_signature(
+        method='GET',
+        request_url=request_url,
+        options={
+            'user': my_user,
+            'auth_params': None}
+    )
+
+    headers={'Authorization': authorization_header, 'Accept':'application/atom+json; charset=utf8'}
+    try:
+        r = requests.get(request_url, headers=headers)
+        r.raise_for_status()
+
+    except requests.exceptions.HTTPError as err:
+        print("Read failed. " + str(err.response.status_code))
+
+    #loads response into object;returns oclc number status
+    return json.loads(r.content)["content"]["holdingCurrentlySet"]
