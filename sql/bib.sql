33b7318d05a6fb6f89900fadfdabfc480a150b4f
craigboman <craig.boman@gmail.com>
2019-04-16 15:43:25 -0400

    finalized version of all bib counts for vendor estimates

--- sql/bib.sql
+++ sql/bib.sql
@@ -1,7 +1,15 @@
-﻿SELECT
+﻿SELECT 
 *
--- count(*)  --total OCLC numbers = 1,530,387
--- DISTINCT(p.index_entry) --should only be OCLC numbers
+-- count(*)  --total OCLC numbers = 1,387,592
+-- count(DISTINCT(p.index_entry)) --should only be OCLC numbers
+
+-- for the greenglass project prep
+-- DISTINCT(r.record_type_code || r.record_num || 'a') AS bib_rec
+-- count(DISTINCT(r.record_type_code || r.record_num || 'a')) AS bib_rec
+
+-- DISTINCT(l.location_code)  --unique list of location codes
+-- l.location_code
+
 FROM
 sierra_view.phrase_entry AS p
 JOIN
@@ -12,27 +20,52 @@ JOIN
 sierra_view.bib_record AS b
 ON
 b.record_id = r.id
+JOIN
+sierra_view.bib_record_location AS l
+ON
+l.bib_record_id = b.record_id
 
 
 -- sierra_view.item_record AS i 
 
 WHERE
-p.index_tag = 'o' 
+p.index_tag = 'o' --excludes anything that isn't an oclc number in theory
 AND 
 p.varfield_type_code IS DISTINCT FROM 'y' -- excludes 24k
 AND 
-r.deletion_date_gmt IS NULL 
+r.deletion_date_gmt IS NULL --excludes deleted records
 AND
-r.campus_code = '' -- excludes virtual metadata
+r.campus_code = '' -- excludes virtual metadata from Ohiolink
 AND 
 r.creation_date_gmt IS NOT NULL  --excludes deleted records
 AND
 b.bcode2 != '@' --exclude eresources
+AND
+b.bcode1 = 'm' --exclude serials
+AND
+l.location_code != 'onl' -- excludes more eresources
+AND
+l.location_code -- = 'swdep'
+IN 
+('kngl', --KING MAIN
+'imc', --YOU KNOW
+'aal', --ART/ARCH
+'spe', --SPEC
+'mic', --SPEC
+'scl', --BEST
+'mul', --MUSIC
+'ref', --KING REF
 
 
--- i.location_code != 'kngle'
+--locations to exclude in production
+	'doc','mdl','hal','swdep', 'multi')
+
+
+-- AND  --can't seem to exclude online gov docs easily
+-- l.location_code NOT IN 
+-- ('doc','hal','swdep','mdl')
+
 -- i.item_status_code != -- in copy cat (23 or n) ?!
--- AND i.location_code IN ('kngl','kng') -- over-limits
 -- r.record_type_code = 'i'
 
 -- for production monthly
@@ -40,4 +73,4 @@ b.bcode2 != '@' --exclude eresources
 
 
 LIMIT 100
-
+-- ORDER BY bib_rec DESC
