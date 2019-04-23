b6b1d8b5ff59cc7d452e880c4d6f965988cfe815
craigboman <craig.boman@gmail.com>
2019-04-23 16:12:24 -0400

    working psycopg2 connection and returning results; mapping out sqlName to main connection

--- python/airflow.py
+++ python/airflow.py
@@ -13,5 +13,37 @@ def sqlImport(sqlName):
     #not returning exact file contents
     # sqlName = 'mdl-min.sql'
 
-def pgsqlConnect():
-    
+# def main(password):
+def main(password,sqlName):
+    import psycopg2, sys
+    try:
+        connection = psycopg2.connect(user = "user",
+                                      password = password,
+                                      host = "host",
+                                      port = "port",
+                                      database = "iii")
+        cursor = connection.cursor()
+        # Print PostgreSQL Connection properties
+        print ( connection.get_dsn_parameters(),"\n")
+        # Print PostgreSQL version
+        cursor.execute(sqlName)
+        # cursor.execute("SELECT * FROM sierra_view.item_record LIMIT 100;")
+        records = cursor.fetchall()
+        # print("You are connected to - ", record,"\n")
+        results = []
+        for row in records:
+           #print("Item", row, "\n")
+           results.append(row)
+        return results
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    finally:
+        #closing database connection.
+            if(connection):
+                cursor.close()
+                connection.close()
+                print("PostgreSQL connection is closed")
+
+
+# if __name__ == "__main__":
+# 	main()
