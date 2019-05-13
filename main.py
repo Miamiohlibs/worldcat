03d696d4ecc4cc220e4bb9c3c34bc688c5056dbf
craigboman <craig.boman@gmail.com>
2019-05-13 15:03:51 -0400

    restructure; move dbWriter out of main(); had it working for a moment

--- main.py
+++ main.py
@@ -40,50 +40,9 @@ def main(password,sqlName):
             connection.close()
             print("PostgreSQL connection is closed")
 
-# if __name__ == "__main__":
-# 	results = main(args,kwargs)
-def dbWriter(results):
-    from random import uniform
-    import sqlite3, time, datetime
-    from stati.holdingStatus import status
-    for i in results:
-        try:
-            test = status(i)
-            if test:
-                print('status checked')
-            else:
-                print('status read failed')
-            conn = sqlite3.connect('Oxford.db')
-            c = conn.cursor()
-            create_db()
-            data_entry(test)
-            print('db write successful')
-        else:
-            print('db write failed')
-        # mul.append([i,test])
-        # print(len(mul))
-        wait = uniform(.2,3)
-        time.sleep(wait)
-
-
-# https://python-forum.io/Thread-insert-list-into-sqlite3
-
-def create_db():
-    c.execute('CREATE TABLE IF NOT EXISTS test(oclc,status)')
-
-def data_entry(i):
-    c.execute("INSERT INTO test VALUES({},{})".format(i[0],i[1]))
-    conn.commit()
-    # c.close()
-
-def dynamic_data_entry():
-    unix = time.time()
-    date = str(datetime.datetime.fromtimestamp(unix).strftime('%m-%d'))
-    value = random.randrange(0,len(words))
-    c.execute("INSERT INTO foobar (unix,datestamp,value) VALUES(?,?,?)",
-    (unix, date, value))
-    conn.commit()
-
-def db_close(c, conn):
-    c.close()
-    conn.close()
+if __name__ == "__main__":
+	# results = main(args,kwargs)
+    import importlib
+    from importlib import reload
+    importlib.reload(python.dbWriter)
+    from python.dbWriter import dbWriter
---
+++ python/dbWriter.py
@@ -0,0 +1,49 @@
+def dbWriter(results):
+    from random import uniform
+    import sqlite3, time, datetime
+    from stati.holdingStatus import status
+    conn = sqlite3.connect('../Oxford.db')
+    c = conn.cursor()
+    for i in results:
+        try:
+            test = status(i)
+            if test:
+                print('status checked', test)
+            else:
+                print('status read failed')
+            c.execute('CREATE TABLE IF NOT EXISTS test(oclc,status)')
+            try:
+                c.execute("INSERT INTO test VALUES({},{})".format(test[0],test[1]))
+                conn.commit()
+                print('db write successful')
+            except:
+                print('db write failed')
+        except:
+            ('nothing worked, throwing in the towel')
+        wait = uniform(.2,3)
+        time.sleep(wait)
+        # mul.append([i,test])
+        # print(len(mul))
+
+
+# https://python-forum.io/Thread-insert-list-into-sqlite3
+
+def create_db():
+    c.execute('CREATE TABLE IF NOT EXISTS test(oclc,status)')
+
+def data_entry(i):
+    c.execute("INSERT INTO test VALUES({},{})".format(i[0],i[1]))
+    conn.commit()
+    # c.close()
+
+def dynamic_data_entry():
+    unix = time.time()
+    date = str(datetime.datetime.fromtimestamp(unix).strftime('%m-%d'))
+    value = random.randrange(0,len(words))
+    c.execute("INSERT INTO foobar (unix,datestamp,value) VALUES(?,?,?)",
+    (unix, date, value))
+    conn.commit()
+
+def db_close(c, conn):
+    c.close()
+    conn.close()
