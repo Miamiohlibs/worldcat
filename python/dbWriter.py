0b63a387d7cfdd9c9b0e1a5f6cca9dc519b407b3
craigboman <craig.boman@gmail.com>
2019-05-14 14:01:02 -0400

    successful write; mapping out multi-thread

--- python/dbWriter.py
+++ python/dbWriter.py
@@ -11,20 +11,20 @@ def dbWriter(results):
                 print('status checked', test)
             else:
                 print('status read failed')
-            # c.execute('CREATE TABLE IF NOT EXISTS test(oclc,status)')
-            try:
-                c.execute("INSERT INTO test VALUES({},{})".format(i,test))
-                conn.commit()
-                print('db write successful')
-            except:
-                print('db write failed')
         except:
             ('nothing worked, throwing in the towel')
         wait = uniform(.2,2)
         time.sleep(wait)
         # mul.append([i,test])
         # print(len(mul))
-
+        c.execute('CREATE TABLE IF NOT EXISTS test(oclc,status)')
+        # should not need to create table but it doesn't work without it
+        try:
+            c.execute("INSERT INTO test VALUES({},{})".format(i,test))
+            conn.commit()
+            print('db write successful')
+        except:
+            print('db write failed')
 
 
 # https://python-forum.io/Thread-insert-list-into-sqlite3
@@ -48,3 +48,25 @@ def dynamic_data_entry():
 def db_close(c, conn):
     c.close()
     conn.close()
+
+def multi(one,two):
+    import multiprocessing
+    # creating processes
+    p1 = multiprocessing.Process(target=dbWriter, args=(one[:3], ))
+    p2 = multiprocessing.Process(target=dbWriter, args=(two[:3], ))
+    # starting process 1
+    p1.start()
+    # starting process 2
+    p2.start()
+    # wait until process 1 is finished
+    p1.join()
+    # wait until process 2 is finished
+    p2.join()
+    # both processes finished
+    print("Done!")
+
+if __name__ == "__main__":
+    one = ['27429232', '32778355', '968141', '39381268', '7119961', '575236', '16715413', '558486', '774160330', '7430088']
+    two = ['17257924', '13845695', '21972508', '48070545', '34281492', '52511400', '51182403', '43396865', '48195094', '32075467']
+    #dbWriter(one)
+    multi(one,two)
