30915f033e9613ae675baafd0caa4714132dd2c8
craigboman <craig.boman@gmail.com>
2019-05-21 09:53:40 -0400

    deprecated multiprocess function

--- python/dbWriter.py
+++ python/dbWriter.py
@@ -81,20 +81,3 @@ if __name__ == "__main__":
 
     # use multiPool to pool processes
     # multiPool(one,two)
-
-# deprecated; multiple sql writes don't work
-# def multi(one,two):
-#     import multiprocessing
-#     # creating processes
-#     p1 = multiprocessing.Process(target=dbWriter, args=(one[:3], ))
-#     p2 = multiprocessing.Process(target=dbWriter, args=(two[:3], ))
-#     # starting process 1
-#     p1.start()
-#     # starting process 2
-#     p2.start()
-#     # wait until process 1 is finished
-#     p1.join()
-#     # wait until process 2 is finished
-#     p2.join()
-#     # both processes finished
-#     print("Done!")
