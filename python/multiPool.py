07b8062c9e8b1fb4e51dceae2cfcc85468437a19
craigboman <craig.boman@gmail.com>
2019-05-21 16:26:47 -0400

    demolished previous pooled script; sketching out shared list locks and db write locks

--- python/multiPool.py
+++ python/multiPool.py
@@ -1,48 +1,44 @@
-# YouTube Link: https://www.youtube.com/watch?v=u2jTn-Gj2Xw
+import multiprocessing
 
-# One can create a pool of processes which will carry out tasks submitted to
-# it with the Pool class.
+# function to withdraw from account
+def withdraw(balance, lock):
+# def read(balance,lock): #read lock
+    lock.acquire()
+    del balance.value[:1]
+    lock.release()
 
-# A process pool object which controls a pool of worker processes to which
-# jobs can be submitted. It supports asynchronous results with timeouts and
-# callbacks and has a parallel map implementation.
+# function to deposit to account
+def deposit(balance, lock):
+# def dbWriter(batch,dbName,lock): #write lock
+    lock.acquire()
+    del balance.value[:1]
+    lock.release()
 
-import time
-from multiprocessing import Pool
-from dbWriter import dbWriter, batchStatus
+def perform_transactions():
 
-def sum_square(ten):
-    batch = batchStatus(ten)
-    dbWriter(batch)
+    # initial balance (in shared memory)
+    balance =  multiprocessing.Manager().list(results)
 
+    # creating a lock object
+    lock = multiprocessing.Lock()
+    # write = multiprocessing.Lock()
 
-def sum_square_with_mp(ten):
+    # creating new processes
+    p1 = multiprocessing.Process(target=withdraw, args=(balance,lock))
+    p2 = multiprocessing.Process(target=deposit, args=(balance,lock))
 
-    start_time = time.time()
-    p = Pool()
-    result = p.map(sum_square, ten)
+    # starting processes
+    p1.start()
+    p2.start()
 
-    p.close()
-    p.join()
+    # wait until processes are finished
+    p1.join()
+    p2.join()
 
-    end_time = time.time() - start_time
+    # print final balance
+    print("Final balance = {}".format(balance.value))
 
-    print(f"Processing {len(ten)} numbers took {end_time} time using multiprocessing.")
-
-
-def sum_square_no_mp(ten):
-
-    start_time = time.time()
-    result = []
-
-    for i in ten:
-        result.append(sum_square(i))
-    end_time = time.time() - start_time
-
-    print(f"Processing {len(ten)} numbers took {end_time} time using serial processing.")
-
-
-if __name__ == '__main__':
-    ten = ['27429232', '32778355', '968141', '39381268', '7119961', '575236', '16715413', '558486', '774160330', '7430088']
-    sum_square_with_mp(ten)
-    sum_square_no_mp(ten)
+if __name__ == "__main__":
+    for _ in range(10):
+        # perform same transaction process 10 times
+        perform_transactions()
