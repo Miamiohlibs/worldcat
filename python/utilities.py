ff688d0cc88083ce5e59ac324f5e5e9d6aff3456
craigboman <craig.boman@gmail.com>
2019-05-24 15:29:01 -0400

    cleaning up; deprecated shredder()

--- python/utilities.py
+++ python/utilities.py
@@ -25,44 +25,5 @@ def sortingHat(slice):
     return good, bad
     print(good, bad)
 
-# slice = ['28633839', '29260', '21972508', '12555624', '32609688', '32075467', '313365654', '11262171', '28384097', '18886825', '13409asdf']
-# good,bad = sortingHat(results)
-
 # test criteria
     # results = main('passwd', sqlImport('main-campus-min.sql'))
-
-def shredder(passwd):
-    import threading, os
-    good,bad = [],[]
-
-    results = main(passwd, sqlImport('main-campus-min.sql'))
-
-    queue1 = results[:50]
-    queue2 = results[50:100]
-
-    # print ID of current process
-    print("ID of process running main program: {}".format(os.getpid()))
-
-    # print name of main thread
-    print("Main thread name: {}".format(threading.main_thread().name))
-
-    # creating threads
-    t1 = threading.Thread(target=sortingHat, name='t1', args=(queue1,))
-    t2 = threading.Thread(target=sortingHat, name='t2', args=(queue2,))
-
-        # starting threads
-    t1.start()
-    t2.start()
-
-    # wait until all threads finish
-    t1.join()
-    t2.join()
-
-    # print(len(good),len(bad))
-
-    # return(good,bad)
-
-if __name__ == "__main__":
-    import sys
-    passwd = sys.argv[1]
-    shredder(passwd)
