a8605a8e61383cc5d82408e5702fccaecd1c10f1
craigboman <craig.boman@gmail.com>
2019-06-06 12:01:00 -0400

    try except added

--- python/utilities.py
+++ python/utilities.py
@@ -3,11 +3,14 @@
 def sqlImport(sqlName):
     # opens a local saved .sql file and returns the query
     # query must be in a minified format without carriage returns or comments
-    with open('sql/'+sqlName, 'r', encoding='utf-8-sig') as f:
-        content = f.read()
-        content = content.replace('\n',' ')
-        f.close()
-    return content
+    try:
+        with open('sql/'+sqlName, 'r', encoding='utf-8-sig') as f:
+            content = f.read()
+            content = content.replace('\n',' ')
+            f.close()
+        return content
+    except:
+        return 'sql error'
     #not returning exact file contents
     # sqlName = 'mdl-min.sql'
 
